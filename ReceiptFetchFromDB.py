c9722a325f49ad3bf08ac04e695a0a9d139350c0
Salvi0ne <152185412+Salvi0ne@users.noreply.github.com>
2024-08-30 10:23:27 +0800

    Update ReceiptFetchFromDB.py

--- ReceiptFetchFromDB.py
+++ ReceiptFetchFromDB.py
@@ -1,33 +1,27 @@
-# fetch_receipts.py
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app import Receipt  # Import the Receipt model from your app
+from app import Receipt
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
 load_dotenv()
 
-# Get the database URL from environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECURITY_KEY = os.getenv("SECURITY_KEY", "123456789qwertyuiop")
 
 def fetch_receipts():
-    # Create an engine and session
     engine = create_engine(DATABASE_URL)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     try:
-        # Query all receipts
         receipts = session.query(Receipt).all()
 
-        # Print receipts in the specified format
         print("Fetched Receipts:")
         print("=" * 30)
         for receipt in receipts:
             print(f"receipt id: {receipt.id}")
-            print(f"date: {receipt.date.strftime('%Y-%m-%d')}")  # Format date as YYYY-MM-DD
+            print(f"date: {receipt.date.strftime('%Y-%m-%d')}")
             print(f"total: {receipt.total:.2f}")
             print(f"category: {receipt.category}")
             print("=" * 30)
